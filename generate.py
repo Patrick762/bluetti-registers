eb6cb41928ce1d126d3003c72ff60824d7acd934
Patrick762 <lutzpatti86@gmail.com>
2026-08-21 15:39:29 +0200

    use one file for version specific fields

--- generate.py
+++ generate.py
@@ -2,7 +2,7 @@ import requests
 import json
 from jsonschema import validate
 
-from helpers import getDevicesBluetooth, getDevicesModbusTcp
+from helpers import getDevicesModbusTcp, getDevicesV1Bluetooth, getDevicesV2Bluetooth, getBaseV1Bluetooth, getBaseV2Bluetooth
 
 print("Loading devices list schema")
 
@@ -10,19 +10,38 @@ schema = requests.get("https://raw.githubusercontent.com/Patrick762/bluetti-regi
 
 print("Getting device files")
 
-device_files = getDevicesBluetooth()
+# region Bluetooth
 
 result = []
 
-for f in device_files:
-    print(f"Loading device definition {f}")
+# Load base fields
+base_fields_v1 = list()
+base_fields_v2 = list()
 
-    with open(f, "r") as f:
-        data = json.load(f)
-        del data["$schema"]
-        result.append(data)
+with open(getBaseV1Bluetooth(), "r") as f:
+    data = json.load(f)
+    base_fields_v1 = data["fields"]
 
-    print("added to result")
+with open(getBaseV2Bluetooth(), "r") as f:
+    data = json.load(f)
+    base_fields_v2 = data["fields"]
+
+# Load device files
+for i, fun in enumerate([getDevicesV1Bluetooth, getDevicesV2Bluetooth]):
+    device_files = fun()
+    base_fields: list = base_fields_v1 if i == 0 else base_fields_v2
+
+    for f in device_files:
+        print(f"Loading device definition {f}")
+
+        with open(f, "r") as f:
+            data = json.load(f)
+            del data["$schema"]
+            for bf in base_fields:
+                data["fields"].append(bf)
+            result.append(data)
+
+        print("added to result")
 
 print("Validating output")
 
@@ -32,6 +51,8 @@ print("Writing result to file")
 
 with open("./bluetooth.json", "w") as f:
     f.write(json.dumps(result))
+
+# region Modbus TCP
 
 device_files = getDevicesModbusTcp()
 
--- helpers.py
+++ helpers.py
@@ -1,17 +1,34 @@
 from os import listdir
 from os.path import isfile, join
 
-def getDevicesBluetooth():
+def is_not_base(path: str):
+    return not path.endswith("base.json")
+
+def getDevicesV1Bluetooth():
     v1_dir = "./bluetooth/v1"
     v1_devices = [join(v1_dir, f) for f in listdir(v1_dir) if isfile(join(v1_dir, f))]
 
+    return filter(is_not_base, v1_devices)
+
+def getDevicesV2Bluetooth():
     v2_dir = "./bluetooth/v2"
     v2_devices = [join(v2_dir, f) for f in listdir(v2_dir) if isfile(join(v2_dir, f))]
 
-    return v1_devices + v2_devices
+    return filter(is_not_base, v2_devices)
+
+def getDevicesBluetooth():
+    return getDevicesV1Bluetooth() + getDevicesV2Bluetooth()
+
+def getBaseV1Bluetooth():
+    v1_dir = "./bluetooth/v1"
+    return join(v1_dir, "base.json")
+
+def getBaseV2Bluetooth():
+    v2_dir = "./bluetooth/v2"
+    return join(v2_dir, "base.json")
 
 def getDevicesModbusTcp():
     v1_dir = "./modbus-tcp/v1"
     v1_devices = [join(v1_dir, f) for f in listdir(v1_dir) if isfile(join(v1_dir, f))]
 
-    return v1_devices
+    return filter(is_not_base, v1_devices)

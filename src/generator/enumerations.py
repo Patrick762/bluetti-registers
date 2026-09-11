8d9f89c98aeb5f264ad68ac5f86c65aa14e426b3
Patrick762 <lutzpatti86@gmail.com>
2026-09-11 15:33:58 +0200

    added datatypes and scalings csv

---
+++ src/generator/enumerations.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class DataType(Enum):
+    UINT = "uint"
+    INT = "int"
+    ENUM = "enum"
+    BOOL = "bool"
+    STRING = "string"
+    SWSTRING = "swstring"
+    VERSION = "version"
+    SERIAL = "serial"
+    TIME = "time"
